6d6f07a68ed11c32f42cec5c3afed77c69733269
thekshitiz <thekshitizrai@gmail.com>
2025-04-11 22:22:33 +0545

    project init

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+// filepath: postcss.config.js
+/** @type {import('postcss').Config} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
