c958ffb9c7943301a816d7505778bba05d7ab2f3
thekshitiz <thekshitizrai@gmail.com>
2025-04-13 15:08:10 +0545

    fix: contact submission message

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
                 <Navbar />
                 <main>{children}</main>
                 <Footer />
+                <Toaster />
             </body>
         </html>
     )
